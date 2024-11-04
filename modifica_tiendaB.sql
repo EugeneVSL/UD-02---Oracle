cc291e116d6a7b832576aa8f981578645e679432
Eugene <eugenevasile@gmail.com>
2024-11-04 18:54:35 +0100

    Small update
    Small update

--- modifica_tiendaB.sql
+++ modifica_tiendaB.sql
@@ -6,15 +6,11 @@ ALTER TABLE stock RENAME TO PRODXTIENDAS;
 ALTER TABLE producto DROP CONSTRAINT producto_familia_fk;
 DROP TABLE familia;
 
--- Estos passos no se pueden ejecutar en este script. Necesitamos privilegios de SYS
--- ------------------------------------------------------------------------------
--- Crea un usuario llamado C##INVITADO siguiendo los pasos de la unidad 1 y dale 
--- todos los privilegios sobre la tabla PRODUCTO.
--- CONNECT sys AS sysdba;
+-- Crea un usuario llamado C##INVITADO 
 CREATE USER c##invitado IDENTIFIED BY invitado DEFAULT TABLESPACE users;
 
 -- Dale todos los privilegios sobre la tabla PRODUCTO
-GRANT ALL ON c##eugene.producto to c##invitado;
+GRANT ALL ON c##eugene.producto TO c##invitado;
 
 -- Retira los permisos de modificar la estructura de la tabla y borrar contenido de la tabla PRODUCTO al usuario anterior.
 REVOKE ALL ON c##eugene.producto FROM c##invitado;
